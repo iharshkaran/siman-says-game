8211c351c0bef97c8059ab1ee8aae53d92169652
iharshkaran <harshkaranofficial@gmail.com>
2026-08-30 11:25:33 +0530

    feat: add restart functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let gameSeq = [];
 let userSeq = [];
-let highestLevel = 0;
+let highestLevel = Number(localStorage.getItem("simonHighestLevel")) || 0;
 
 let btns = ["red", "green", "yellow", "purple"]
 
@@ -9,15 +9,21 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener('keypress', () => {
+function startGame() {
     if (started === false) {
-        console.log("Game is started")
         started = true;
 
         levelUp();
     }
+}
+
+document.addEventListener('keypress', () => {
+    startGame();
+    startBtn.innerText = "Restart Game";
 })
 
+
+const startBtn = document.querySelector("#start-btn");
 
 function btnFlash(btn) {
     btn.classList.add("flash");
@@ -30,54 +36,49 @@ function btnFlash(btn) {
 
 function levelUp() {
     level++;
-    highestLevel++;
     userSeq = [];
 
     h2.innerText = `Level ${level}`;
-    let randIdx = Math.floor(Math.random() * 3);;
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`)
     gameSeq.push(randColor)
-    console.log(`gameSeq : ${gameSeq}`)
     btnFlash(randBtn);
 }
 
 
-function checkAns(idx){
-    if(userSeq[idx] === gameSeq[idx]){
+function checkAns(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
 
-        if(userSeq.length === gameSeq.length){
+        if (userSeq.length === gameSeq.length) {
             setTimeout(levelUp, 200);
         }
 
     } else {
-        
-        showHighestLevel();
-        
-        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
-        reset();
+
+        endGame();
     }
 }
 
 
-function btnPress(){
-    let btn = this;
-    const userColor = btn.getAttribute('id');
-    userSeq.push(userColor)
-    console.log(`userSeq : ${userSeq}`);
-    
-    
-    checkAns(userSeq.length-1)
+function btnPress() {
+    if (started) {
+        let btn = this;
+        const userColor = btn.getAttribute('id');
+        userSeq.push(userColor)
+
+        checkAns(userSeq.length - 1)
+    }
 }
 
 
 let allBtns = document.querySelectorAll('.btn');
 
-for(btn of allBtns){
+for (btn of allBtns) {
     btn.addEventListener('click', btnPress)
 }
 
-function reset(){
+function reset() {
     started = false;
     level = 0;
     userSeq = []
@@ -90,8 +91,53 @@ function showHighestLevel() {
     gameOver.innerHTML = "";
 
     let h2 = document.createElement("h2");
-    h2.innerText = `Highest Level: ${level}`;
+    h2.innerText = `Highest Level: ${highestLevel}`;
     h2.classList.add("highest-level");
 
     gameOver.appendChild(h2);
 }
+
+
+function endGame() {
+    let currentScore = level - 1;
+
+    // compare current and highest level
+    if (currentScore > highestLevel) {
+        highestLevel = level - 1;
+        localStorage.setItem("simonHighestLevel", highestLevel);
+    }
+
+    showHighestLevel();
+
+    h2.innerHTML = `Game Over! Your score was <b>${level - 1}</b><br>Press any key to start.`;
+    startBtn.style.display = "inline-block";
+    reset();
+}
+
+const restartPopup = document.querySelector("#restart-popup");
+const cancelBtn = document.querySelector("#cancel-btn");
+const confirmRestart = document.querySelector("#confirm-restart");
+
+startBtn.addEventListener("click", () => {
+    if (!started) {
+        startGame();
+        started = true;
+        startBtn.innerText = "Restart Game";
+    } else {
+        restartPopup.style.display = "flex";
+    }
+});
+
+cancelBtn.addEventListener("click", () => {
+    restartPopup.style.display = "none";
+});
+
+confirmRestart.addEventListener("click", () => {
+    restartPopup.style.display = "none";
+
+    reset();
+    startGame();
+
+    started = true;
+    startBtn.innerText = "Restart Game";
+});
